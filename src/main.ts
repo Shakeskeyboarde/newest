e360c586782ef603ab335038894a44dc45c259ea
Chris Ackerman <chris@topher.land>
2020-10-01 10:58:11 -0700

    Only confirm if updates are available.

--- src/main.ts
+++ src/main.ts
@@ -7,37 +7,20 @@ import { readPackage } from "./readPackage";
 import { resolveMinVersions } from "./resolveMinVersions";
 import { resolveUpdateVersions } from "./resolveUpdateVersions";
 import { selectUpdates } from "./selectUpdates";
+import { confirmInteractive } from "./confirmInteractive";
 
 export default async () => {
-  let confirmAnswered = false;
-
-  const confirmed = await Promise.race([
-    prompts({
-      type: "confirm",
-      name: "value",
-      message: "Do you want to update packages?",
-    }).finally(() => {
-      confirmAnswered = true;
-    }),
-    delay(5000).then(() => {
-      if (!confirmAnswered) console.log();
-    }),
-  ]);
-
-  if (!confirmed || !confirmed?.value) {
-    if (confirmed == null) {
-      console.log(chalk.gray("Confirmation timed out."));
-    }
-
-    process.exit(0);
-  }
-
   const { indent, pkg } = await readPackage();
   const minVersions = resolveMinVersions(pkg);
   const updateVersions = await resolveUpdateVersions(minVersions);
 
   if (updateVersions.size === 0) {
     console.log(chalk.gray("All packages are up to date."));
+    process.exit(0);
+  }
+
+  if (!(await confirmInteractive())) {
+    console.log(chalk.gray(`No changes have been made.`));
     process.exit(0);
   }
 
