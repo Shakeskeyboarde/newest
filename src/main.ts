b26a741959dd57ed528ac6b02f19d34c3501b964
Chris Ackerman <chris@topher.land>
2020-10-01 10:44:06 -0700

    Added a confirmation prompt which times out for non-interactive use.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,37 @@
-import chalk from 'chalk';
-import prompts from 'prompts';
-import { applyUpdates } from './applyUpdates';
-import { readPackage } from './readPackage';
-import { resolveMinVersions } from './resolveMinVersions';
-import { resolveUpdateVersions } from './resolveUpdateVersions';
-import { selectUpdates } from './selectUpdates';
+import chalk from "chalk";
+import delay from "delay";
+import prompts from "prompts";
+import AbortController from "abort-controller";
+import { applyUpdates } from "./applyUpdates";
+import { readPackage } from "./readPackage";
+import { resolveMinVersions } from "./resolveMinVersions";
+import { resolveUpdateVersions } from "./resolveUpdateVersions";
+import { selectUpdates } from "./selectUpdates";
 
 export default async () => {
+  let confirmAnswered = false;
+
+  const confirmed = await Promise.race([
+    prompts({
+      type: "confirm",
+      name: "value",
+      message: "Do you want to update packages?",
+    }).finally(() => {
+      confirmAnswered = true;
+    }),
+    delay(5000).then(() => {
+      if (!confirmAnswered) console.log();
+    }),
+  ]);
+
+  if (!confirmed || !confirmed?.value) {
+    if (confirmed == null) {
+      console.log(chalk.gray("Confirmation timed out."));
+    }
+
+    process.exit(0);
+  }
+
   const { indent, pkg } = await readPackage();
   const minVersions = resolveMinVersions(pkg);
   const updateVersions = await resolveUpdateVersions(minVersions);
